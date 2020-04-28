40f3e74232a782d84ba3c545cd52f270fcbce46a
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-04-28 09:32:07 -0400

    structure and format

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('projects/', views.projects),
+    path('resume/', views.resume),
+    path('cp/', views.cp)
 ]
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'blog/base.html')
+
+def projects(request):
+    return render(request, 'blog/projects.html')
+
+def resume(request):
+    return render(request, 'blog/resume.html')
+
+def cp(request):
+    return render(request, 'blog/cp.html')
