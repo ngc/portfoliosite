fae300cf12038d63a059fe60c7793ab83832ad2c
thaniel-c <contact.nathancoulas@gmail.com>
2020-07-02 22:44:36 -0400

    popen and nohup staging

--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,7 @@ def lastfm(request):
                     process_argument = "-u " + graph_instance.user1
                 
                 devnull = open(os.devnull, 'wb')
-                subprocess.run(["nohup", sys.executable, '/home/nathan/portfoliosite/lastfmgraph.py', process_argument])
+                subprocess.Popen(["nohup", sys.executable, '/home/nathan/Development/portfoliosite/lastfmgraph.py', process_argument])
                 
                 return redirect('../lastfmgraph')
     else:
